cf57b958504a0017156fc95cd0b61665adcad04c
unknown <Doodle23@yahoo.com>
2022-04-04 11:38:10 +0900

    started navbar (following tutorial )

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Products from './components/products/Products';
+import navbar from './components/navbar/navbar';
+
+const App = () => {
+  return (
+    <div>
+
+        <Products/>
+    </div>
+  )
+}
+
+export default App;
